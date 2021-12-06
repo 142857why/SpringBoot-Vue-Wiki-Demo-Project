2826b056c60f8773fed880c2800bfa5781a48fc8
Rui Zhou <zhourui_1997@126.com>
2021-12-06 19:07:24 +0100

    3-4 导入myBatis; 引入Mapper; 开发test/list接口展示test数据表中的数据

--- src/main/java/com/robbie/wiki/controller/TestController.java
+++ src/main/java/com/robbie/wiki/controller/TestController.java
@@ -1,8 +1,14 @@
 package com.robbie.wiki.controller;
 
+import com.robbie.wiki.domain.Test;
+import com.robbie.wiki.service.TestService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 public class TestController {
@@ -10,10 +16,17 @@ public class TestController {
     @Value("${test.hello:\nTEST}")
     private String testHello;
 
+    @Resource
+    private TestService testService;
 
     @RequestMapping("/hello")
     public String hello() {
 //        System.out.println("被访问啦！");
         return "Hello, World" + testHello;
+    }
+
+    @GetMapping("/test/list")
+    public List<Test> list() {
+        return testService.list();
     }
 }
---
+++ src/main/java/com/robbie/wiki/mapper/TestMapper.java
@@ -0,0 +1,10 @@
+package com.robbie.wiki.mapper;
+
+import com.robbie.wiki.domain.Test;
+
+import java.util.List;
+
+public interface TestMapper {
+    public List<Test> list();
+
+}
