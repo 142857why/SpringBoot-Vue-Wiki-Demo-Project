d1531ae69ee5b90706a5ff641d1e4fb6bea17700
Rui Zhou <zhourui_1997@126.com>
2021-12-06 01:49:48 +0100

    2-8 自定义配置项并获取

--- src/main/java/com/robbie/wiki/controller/TestController.java
+++ src/main/java/com/robbie/wiki/controller/TestController.java
@@ -1,14 +1,19 @@
 package com.robbie.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @Value("${test.hello:\nTEST}")
+    private String testHello;
+
+
     @RequestMapping("/hello")
     public String hello() {
 //        System.out.println("被访问啦！");
-        return "Hello, World";
+        return "Hello, World" + testHello;
     }
 }
